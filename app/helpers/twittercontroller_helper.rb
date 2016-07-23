8bbefbb57c573606eea518fd21aecd7909841767
Ina <inatsetsova@gmail.com>
2016-07-23 16:19:27 +0100

    adds relevant tweet information

--- app/helpers/twittercontroller_helper.rb
+++ app/helpers/twittercontroller_helper.rb
@@ -1,9 +1,14 @@
 module TwittercontrollerHelper
 
-def justin_bieber
-  $twitter.search("to:justinbieber marry me", result_type: "recent").take(3).collect do |tweet|
-    "#{tweet.user.screen_name}: #{tweet.text}"
+def getting_tweets
+  topics = ["PostRefRacism"]
+
+  $twitter.search(topics.join(","), result_type: "recent").take(1).collect do |tweet|
+
+    "#{tweet.user.name} #{tweet.user.screen_name}: #{tweet.text}....... #{tweet.retweet_count} #{tweet.favorite_count} #{tweet.created_at}"
+
   end
 end
+
 
 end
