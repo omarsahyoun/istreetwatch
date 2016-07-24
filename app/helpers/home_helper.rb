527902fa05279e309e90d7ed77f966cd3ef2f09d
Ina <inatsetsova@gmail.com>
2016-07-24 12:44:34 +0100

    adds like and retweet images

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,20 +1,22 @@
+
+
 module HomeHelper
 
   def getting_tweets
     topics = ["PostRefRacism"]
     @tweets = []
 
-    $twitter.search(topics.join(","), result_type: "recent").take(10).collect do |tweet|
+    $twitter.search(topics.join(","), result_type: "recent").take(15).collect do |tweet|
       @tweet = {  username: tweet.user.name,
                   screen_name: tweet.user.screen_name,
                   screen_name: tweet.user.screen_name,
                   text: tweet.text,
                   retweets: tweet.retweet_count,
                   favorites: tweet.favorite_count,
-                  created_at: tweet.created_at
+                  created_at: tweet.created_at.strftime('%a, %d %b %Y %H:%M:%S'),
+                  uri: tweet.uri
                 }
       @tweets << @tweet
-      # "#{tweet.user.name} #{tweet.user.screen_name}: #{tweet.text}....... #{tweet.retweet_count} #{tweet.favorite_count} #{tweet.created_at}"
 
     end
   end
